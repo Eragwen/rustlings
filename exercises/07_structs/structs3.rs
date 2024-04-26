6f0373f9ce2f177140b0aafeae0cb3754541a87e
Eragwen <gwendal_besnard@hotmail.com>
2024-04-26 10:03:44 +0200

    add structs

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -31,14 +30,17 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
+
+// Pour "is_international", on compare les deux pays pour savoir si la livraison est internationale ou non
+// Pour "get_fees", on va multiplier le poids du colis par le prix/gramme pour obtenir le prix total de la livraison
 
 #[cfg(test)]
 mod tests {
