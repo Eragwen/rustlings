4cc848a598307e45e4da66f4c1d151f511705ab7
Eragwen <gwendal_besnard@hotmail.com>
2024-04-26 10:39:32 +0200

    add strings

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -7,7 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -17,14 +16,25 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    string("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
+
+// Pour "blue", type &str donc on utilise string_slice car c'est une chaine littérale
+// Pour "red", type String donc on utilise string car c'est une chaine allouée grâce à to_string()
+// Pour "hi", type String donc on utilise string car c'est une chaine allouée grâce à String::from()
+// Pour "rust is fun!", type String donc on utilise string car c'est une chaine allouée grâce à to_owned()
+// Pour "nice weather", type String donc on utilise string car c'est une chaine allouée grâce à into()
+// Pour "Interpolation Station", type String donc on utilise string car c'est une chaine allouée grâce à format!()
+// Pour "a", type &str donc on utilise string_slice car c'est une chaine littérale. C'est une référence à une partie de la chaine grâce à &String
+// Pour "hello there", type &str donc on utilise string_slice car c'est une chaine littérale, trim() renvoie une référence à une partie de la chaine
+// Pour "Happy Tuesday!", type String donc on utilise string car c'est une chaine allouée grâce à to_string()
+// Pour "mY sHiFt KeY iS sTiCkY", type String donc on utilise string car c'est une chaine allouée grâce à to_lowercase()
